fcba22e854d702439b0352385bebd0a8b3ae62f6
hao.song_122108 <hao.song@verisilicon.com>
2025-07-03 17:43:00 +0800

    update config for test customPipeline

--- test/testCustomPipeline.groovy
+++ test/testCustomPipeline.groovy
@@ -1,6 +1,8 @@
 @Library('my-jenkins-shared-lib@dev') _
 
-customPipeline.runTask([:]) {
+customPipeline.runTask([
+    archiveArtifacts : 'build/**,test/*.log',
+    ]) {
     stage('Prepare') {
         sh """
         echo GIT_USER: \${GIT_USER}
@@ -10,14 +12,20 @@ customPipeline.runTask([:]) {
         echo SSH_USER: \${SSH_USER}
         """
     }
+    codeSync([
+      repoUrl: 'https://github.com/sunghowe159/FOA-IVFA-for-WSN',
+      branch: 'master',
+      credentialsId: 'haosong_github_user'
+    ])
     stage('Build') {
         runCmd('''
-        echo 'start building'
+        echo 'start building' > build/build.log
+        uname -a
         ''')
     }
     stage('Test') {
         runCmd('''
-        echo 'start testing'
+        echo 'start testing' > build/test.log
         ''')
     }
 }
