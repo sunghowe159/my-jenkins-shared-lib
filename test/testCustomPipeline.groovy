f6184dff8dedbc07aa71aa80761843470b43b997
hao.song_122108 <hao.song@verisilicon.com>
2025-07-03 17:43:47 +0800

    update config for test customPipeline

--- test/testCustomPipeline.groovy
+++ test/testCustomPipeline.groovy
@@ -19,6 +19,7 @@ customPipeline.runTask([
     ])
     stage('Build') {
         runCmd('''
+        mkdir -p build,test
         echo 'start building' > build/build.log
         uname -a
         ''')
